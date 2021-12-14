caf7e4f760d6d8129cce1e9fa96687ab2d095ca3
shustreek <shustreek77@ya.ru>
2021-12-14 22:36:45 +0300

    init commit

---
+++ helloworld.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// Вызывает функцию std::cout
+void print()
+{
+    // Потому что хотим использовать стандартную библиотеку для печати в консоль и
+    // не использовать какие-либр другие
+    std::cout << "Hello World!\n";
+}
+
+/*
+ * Присваеваем значения переменым и печатаем hello world
+*/
+int main()
+{
+    int x = 100;
+    int y = x + 100;
+    3 + 7;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+}

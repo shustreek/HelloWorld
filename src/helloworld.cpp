b056a5ba955cb22eb7527c7d291bacd685a52717
shustreek <shustreek77@ya.ru>
2021-12-17 00:00:03 +0300

    lesson 14 enter string

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include "Helpers.h"
+#include <string>
 
 int main()
 {
     int a = 1, b = 3;
-    std::cout << "Square of sum a and b: " << squareSumm(a, b);
+    std::cout << "Square of sum a and b: " << squareSumm(a, b) << "\n";
+
+    std::string str;
+    
+    std::cout << "Please, enter something: ";
+    std::getline(std::cin, str);
+
+    std::cout << str << " with length: " << str.length() << "; first: " << str.front() << " and last: " << str.back() << "\n";
 }

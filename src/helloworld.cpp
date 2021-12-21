39df64dc3dbb4b520a36b59823f04411ffdd90c6
shustreek <shustreek77@ya.ru>
2021-12-22 01:17:39 +0300

    создать массив и вывести сумму строки

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -2,6 +2,8 @@
 #include "Helpers.h"
 #include <string>
 
+using namespace std;
+
 int main()
 {
     /*    int a = 1, b = 3;
@@ -13,6 +15,32 @@ int main()
     std::getline(std::cin, str);
 
     std::cout << str << " with length: " << str.length() << "; first: " << str.front() << " and last: " << str.back() << "\n";
-*/
     printNumbers(10, false);
+*/
+    const int N = 5;
+    int array[N][N];
+
+    time_t now = time(0);
+    int day = localtime(&now)->tm_mday;
+    int row = day % N;
+
+    // initialization
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int summ = i + j;
+            array[i][j] = summ;
+            cout << summ << " ";
+        }
+        cout << "\n";
+    }
+
+    // summ of row
+    int summ = 0;
+    for (int i = 0; i < N; i++)
+    {
+        summ += array[row][i];
+    }
+    cout << "Summ of " << row << " row: " << summ << "\n";
 }

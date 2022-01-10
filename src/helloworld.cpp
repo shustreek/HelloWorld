8594ceeccedcbc5119be8761aeb90d93edcb4d88
shustreek <shustreek77@ya.ru>
2022-01-10 21:27:23 +0300

    создать класс

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,46 +1,27 @@
 #include <iostream>
 #include "Helpers.h"
 #include <string>
+#include "Vector.cpp"
 
 using namespace std;
 
+class Example
+{
+private:
+    string text = "something";
+
+public:
+    string getText()
+    {
+        return text;
+    }
+};
+
 int main()
 {
-    /*    int a = 1, b = 3;
-    std::cout << "Square of sum a and b: " << squareSumm(a, b) << "\n";
-
-    std::string str;
-    
-    std::cout << "Please, enter something: ";
-    std::getline(std::cin, str);
-
-    std::cout << str << " with length: " << str.length() << "; first: " << str.front() << " and last: " << str.back() << "\n";
-    printNumbers(10, false);
-*/
-    const int N = 5;
-    int array[N][N];
-
-    time_t now = time(0);
-    int day = localtime(&now)->tm_mday;
-    int row = day % N;
-
-    // initialization
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            int summ = i + j;
-            array[i][j] = summ;
-            cout << summ << " ";
-        }
-        cout << "\n";
-    }
-
-    // summ of row
-    int summ = 0;
-    for (int i = 0; i < N; i++)
-    {
-        summ += array[row][i];
-    }
-    cout << "Summ of " << row << " row: " << summ << "\n";
+    Example example;
+    Vector vector;
+    cout << example.getText() << "\n";
+    vector.Show();
+    cout << "vector length: " << vector.Length() << "\n";
 }
